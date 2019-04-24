f91c17478489c5711010d679a12d3a2106a9b141
makoto.ogawa <makoto.ogawa@smartdrive.co.jp>
2019-04-24 12:51:40 +0900

    first commit

---
+++ lib/danger/lgtmoon.rb
@@ -0,0 +1,11 @@
+require "danger/lgtmoon/version"
+
+module Danger
+  module Lgtmoon
+    def self.greet
+      'Hello World!'
+    end
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/danger/lgtmoon/version.rb
@@ -0,0 +1,5 @@
+module Danger
+  module Lgtmoon
+    VERSION = "0.1.0"
+  end
+end
